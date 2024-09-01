2439f86e5fa846bccd45e5a16e72210b41d62a08
rodygayz12 <161971854+rodygayz12@users.noreply.github.com>
2024-09-01 12:42:36 +0400

    Update webrtc.js

--- webrtc.js
+++ webrtc.js
@@ -17,7 +17,7 @@ const USE_VIDEO = true;
 
 // You can continue using this signalling server or spin up your own.
 // Source to code for thiscan be found at https://github.com/vasanthv/talk/blob/master/signalling-server/index.js
-const SIGNALLING_SERVER = "https://talk-zxrf.onrender.com";
+const SIGNALLING_SERVER = "https://criminal-katha-rodygayz8-766dbdbf.koyeb.app";
 
 let signalingSocket = null; /* our socket.io connection to our webserver */
 let localMediaStream = null; /* our own microphone / webcam */
